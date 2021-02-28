f1709aa278f94b9db65b8bdd1d45d954df3b553e
Hikaruweng <hikaruweng@Hikaruwengs-MacBook-Pro.local>
2021-02-28 15:59:34 +0700

    Finish Part4 Lab2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Part4Lab1
+# Part4Lab2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
 
--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-form',
@@ -10,12 +10,16 @@ export class MyFormComponent implements OnInit {
   form: FormGroup;
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      phoneNo: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      phoneNo: [''],
+      email: ['', Validators.email],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {}
+
+  submitForm() {
+    alert(JSON.stringify(this.form.value));
+  }
 }
